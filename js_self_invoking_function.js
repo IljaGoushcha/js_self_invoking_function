baa8ad0fa7a90c986d3c57e6514c9dc37e4eb8cd
IljaGoushcha <iljagou@yahoo.com>
2015-01-02 10:40:16 -0500

    playing with anonymous functions

--- js_self_invoking_function.js
+++ js_self_invoking_function.js
@@ -4,4 +4,7 @@
   return 2 * x;
 })(4);
 
-
+console.log("***");
+console.log("before setTimeout()");
+setTimeout(function() {console.log("inside setTimeout()");}, 1500);
+console.log("after setTimeout()");
