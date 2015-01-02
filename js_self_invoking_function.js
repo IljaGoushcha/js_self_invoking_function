c13e7ba602f6f909763ef6f9940837fa4f1df427
IljaGoushcha <iljagou@yahoo.com>
2015-01-02 18:48:02 -0500

    playing with callback function

--- js_self_invoking_function.js
+++ js_self_invoking_function.js
@@ -6,5 +6,22 @@
 
 console.log("***");
 console.log("before setTimeout()");
-setTimeout(function() {console.log("inside setTimeout()");}, 1500);
+setTimeout(function() {console.log("inside setTimeout()")}, 750);
 console.log("after setTimeout()");
+
+var tuna = "hey i am a tuna fish!";
+console.log(tuna.typeOf);
+debugger;
+
+console.log("starting Matematika");
+var matematika = {
+  num1: 5,
+  num2: 6,
+  addTwoNumbers: function(num1, num2) {return num1 + num2;}
+};
+
+var num1 = 7;
+var num2 = 5;
+
+console.log(matematika.addTwoNumbers(num1, num2));
+console.log(matematika.addTwoNumbers(function() {return 10}, num2));
