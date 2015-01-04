ab3292ea6def75dd1c9e43c6c959ceebda76023a
IljaGoushcha <iljagou@yahoo.com>
2015-01-04 15:23:20 -0500

    same as last commit, but with .shift()

--- poplarPuzzlers.js
+++ poplarPuzzlers.js
@@ -1,5 +1,5 @@
 var puzzlers = [
-  function ( a ) { console.log("hi"); return 8*a - 10;},
+  function ( a ) { return 8*a - 10;},
   function ( a ) { return (a-3) * (a-3) * (a-3); },
   function ( a ) { return a * a + 4; },
   function ( a ) { return a % 5; }
@@ -12,18 +12,19 @@ var applyAndEmpty = function(startValue, queueOfFunctions) {
   var output;
   var outputFunction;
   var lengthOfQueue = queueOfFunctions.length;
-  console.log((queueOfFunctions[0])(2));
+  // console.log((queueOfFunctions[0])(2));
+  console.log((queueOfFunctions.shift())(2));
   console.log("-----");
 
-  for (var i = 0; i < lengthOfQueue; i++) {
-    outputFunction = queueOfFunctions.shift();
-    console.log(outputFunction);
-    outout = outputFunction(input);
-    console.log(output);
-    input = output;
-  }
-  console.log("***");
-  console.log(output);
+  // for (var i = 0; i < lengthOfQueue; i++) {
+  //   outputFunction = queueOfFunctions.shift();
+  //   console.log(outputFunction);
+  //   output = outputFunction(input);
+  //   console.log(output);
+  //   input = output;
+  // }
+  // console.log("***");
+  // console.log(output);
 }
 
 applyAndEmpty(start, puzzlers);
