3c93e3548c862f2a59fecd1963ee609c65e27b43
IljaGoushcha <iljagou@yahoo.com>
2015-01-04 15:18:02 -0500

    reading anonymous function from array and self invoke with variable

---
+++ poplarPuzzlers.js
@@ -0,0 +1,29 @@
+var puzzlers = [
+  function ( a ) { console.log("hi"); return 8*a - 10;},
+  function ( a ) { return (a-3) * (a-3) * (a-3); },
+  function ( a ) { return a * a + 4; },
+  function ( a ) { return a % 5; }
+];
+var start = 2;
+
+var applyAndEmpty = function(startValue, queueOfFunctions) {
+
+  var input = startValue;
+  var output;
+  var outputFunction;
+  var lengthOfQueue = queueOfFunctions.length;
+  console.log((queueOfFunctions[0])(2));
+  console.log("-----");
+
+  for (var i = 0; i < lengthOfQueue; i++) {
+    outputFunction = queueOfFunctions.shift();
+    console.log(outputFunction);
+    outout = outputFunction(input);
+    console.log(output);
+    input = output;
+  }
+  console.log("***");
+  console.log(output);
+}
+
+applyAndEmpty(start, puzzlers);
