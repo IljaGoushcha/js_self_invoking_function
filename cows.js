cf6b7e8f7c74e5c1d7e0ef35c686ec3f472b0d77
IljaGoushcha <iljagou@yahoo.com>
2015-01-08 10:37:20 -0500

    inheritance with Object.create()

--- cows.js
+++ cows.js
@@ -8,6 +8,14 @@ var canyonCows = [
   {name: "Delilah", type: "cow", hadCalf: null}
 ];
 
+var forestCows = [
+  {name: "Legolas", type: "calf", hadCalf: null},
+  {name: "Gimli", type: "bull", hadCalf: null},
+  {name: "Arwen", type: "cow", hadCalf: null},
+  {name: "Galadriel", type: "cow", hadCalf: null},
+  {name: "Eowyn", type: "cow", hadCalf: "Legolas"}
+];
+
 Array.prototype.countCattle = function(type) {
   var count = 0;
   this.map(function(cow) {
@@ -21,4 +29,15 @@ Array.prototype.countCattle = function(type) {
 console.log(canyonCows.countCattle("cow"));
 console.log(canyonCows.countCattle("calf"));
 console.log(canyonCows.countCattle("bull"));
+
+console.log("*****");
+
+Object.prototype.noCalvesYet = function() {
+  if (this["hadCalf"] == null && this["type"] == "cow") {
+    return true;
+  }
+  return false;
+};
+
+console.log(forestCows[2].noCalvesYet());
 
