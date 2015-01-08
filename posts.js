21c7b5a942b1383817970d6b8a0668fe027c9ed3
IljaGoushcha <iljagou@yahoo.com>
2015-01-08 12:01:21 -0500

    added Fencepost constructor function git push origin master

--- posts.js
+++ posts.js
@@ -1,3 +1,16 @@
+function Fencepost(x, y, postNum) {
+  this.x = x;
+  this.y = y;
+  this.postNum = postNum;
+  this.connectionsTo = [];
+  this.sendRopeTo = function(connectedPost) {
+    this.connectionsTo.push(connectedPost);
+  }
+}
+
+var post11 = new Fencepost(2, 4, 11);
+console.log(post11);
+
 var genericPost = {
   x: 0,
   y: 0,
