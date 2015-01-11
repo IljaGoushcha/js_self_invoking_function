734c56ef08d74d01b087f8c91311556825649035
IljaGoushcha <iljagou@yahoo.com>
2015-01-10 21:34:24 -0500

    tested .before() and .after()

--- traversing_dom/app.js
+++ traversing_dom/app.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
   var message = "<p>1-888-9090</p>";
   $('.tour').on('click', function() {
+    $(this).find('h2').before('<h2>hi</h2>');
     $(this).append(message);
     $(this).find('button').remove();
     $(this).find('div').remove();
