70a557242e86fafec9de3a60c8232b9b5e61e434
IljaGoushcha <iljagou@yahoo.com>
2015-02-04 17:07:55 -0500

    added second hotspot

--- positioning/app.js
+++ positioning/app.js
@@ -1,14 +1,25 @@
-function hideHotSpots() {
+function hideHotSpot1() {
   console.log("hi");
   $('#hotspot-1-message').hide();
 }
-function openHotSpot() {
+function openHotSpot1() {
   console.log("hi");
   $('#hotspot-1-message').show();
 }
+function hideHotSpot2() {
+  console.log("hi");
+  $('#hotspot-2-message').hide();
+}
+function openHotSpot2() {
+  console.log("hi");
+  $('#hotspot-2-message').show();
+}
 
 jQuery(document).ready(function() {
-  hideHotSpots();
-  $('#hotspot-1').on('mouseenter', openHotSpot);
-  $('#hotspot-1').on('mouseleave', hideHotSpots);
+  hideHotSpot1();
+  hideHotSpot2();
+  $('#hotspot-1').on('mouseenter', openHotSpot1);
+  $('#hotspot-1').on('mouseleave', hideHotSpot1);
+  $('#hotspot-2').on('mouseenter', openHotSpot2);
+  $('#hotspot-2').on('mouseleave', hideHotSpot2);
 });
